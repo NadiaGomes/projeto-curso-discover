1a12f754e546e1482efba82a2072f19a51594f3f
Silvio Junior <silvio.fs.junior@gmail.com>
2024-04-24 21:31:42 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  //Pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  //Substituir a imagem
+  if (html.classList.contains("light")) {
+  //Se tiver o light mode, adicionar a imagem light
+    img.setAttribute('src', './assets/avatar-light.png')
+    img.setAttribute('alt', "Foto de Mayk sorrindo, usando óculos escuros, sem barba e fundo degradê do lilás para o azul claro.")
+  } else {
+    //Se tiver sem light mode, manter a imagem normal
+    img.setAttribute('src', './assets/avatar.png')
+    img.setAttribute(
+      "alt",
+      "Foto de Mayk Brito sorrindo, usando óculos e camisa preta, barba e fundo amarelo."
+    )
+  }
+}
